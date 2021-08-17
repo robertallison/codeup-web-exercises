a743bb740916431d0e653f6d3693ce758f3ea798
Robert Allison <robert.r.allison4@gmail.com>
2021-08-17 15:31:07 -0500

    Almost finished with for loops, need to figure out pyramid of numbers

---
+++ js/break_and_continue.js
@@ -0,0 +1,8 @@
+'use strict'
+/* var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+This is just a practice on while loops, actual problems are below
+ */
---
+++ js/for_loops.js
@@ -0,0 +1,44 @@
+"use strict"
+//Below is a function that shows a X10 multiplication table for a user inputted number
+/* function showMultiplicationTable() {
+    var userNum = prompt("Please input a number for the multiplication table")
+    for (var i = 1; i <= 10; i++) {
+        console.log(userNum * i)
+    }
+}
+showMultiplicationTable()
+*/
+//The function below shows 10 random numbers between 20 and 200 and
+//says if it is even or odd
+
+function randNumbers() {
+    for (var i = 1; i <= 10; i++) {
+        var random = Math.floor((Math.random() * 180) + 20);
+        if (random % 2 == 0) {
+            console.log(random + " is even");
+        } else {
+            console.log(random + " is odd");
+        }
+    }
+}
+randNumbers();
+
+
+/*
+function pyramidNum() {
+    for (var i = 1; i < 10; i++){
+        console.log(i)
+    }
+}
+pyramidNum();
+*/
+
+//This function subtracts 5 from 100 till it reaches 5
+/*
+function subtractFive () {
+    for (var i = 0; i < 20; i++){
+        console.log(100 - (5*i))
+    }
+}
+subtractFive();
+ */
