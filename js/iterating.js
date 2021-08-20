77f6e458e551fa643362d6f476ea7e1b39102eba
Robert Allison <robert.r.allison4@gmail.com>
2021-08-20 12:16:10 -0500

    feat: finished iterating arrays, might go back and make the last problem more complex

--- js/iterating.js
+++ js/iterating.js
@@ -13,24 +13,33 @@
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+    console.log(names.length);
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i] + " but in a for loop");
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    names.forEach(function(name){
+        console.log(name + " but this time in a forEach loop");
+    });
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -44,5 +53,13 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-
+    function arrayElements() {
+        for (var i = 0; i < names.length; i++) {
+            if (i != 2) {
+                console.log(i + " " + names[i]);
+            } else
+                console.log("NO")
+        }
+    }
+arrayElements();
 })();
