99217842e488ac267dda226701d3ce316ff09989
Robert Allison <robert.r.allison4@gmail.com>
2021-09-03 09:26:09 -0500

    workin on css and practice

---
+++ js/practice_exercises.js
@@ -0,0 +1,19 @@
+
+/*function tellFortune (numChildren, partnerName, geoLocation, jobTitle){
+
+    var output = "You will have " + numChildren + " children"
+
+    console.log(output);
+    return output
+
+}
+*/
+
+
+
+function calculateDogAge() {
+    var dogAge = prompt("How old is your dog in human years?");
+    var humanAge = dogAge * 7;
+    alert("Your dog is " + humanAge + " years old in dog years!");
+}
+calculateDogAge();
