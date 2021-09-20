fb00c26f3a4b7e164c3e777cd717fb5139b5eac9
Robert Allison <robert.r.allison4@gmail.com>
2021-09-20 10:46:31 -0500

    Finished with in class practice, working on exercises

--- js/practice_exercises.js
+++ js/practice_exercises.js
@@ -11,9 +11,54 @@
 
 
 
-function calculateDogAge() {
+/* function calculateDogAge() {
     var dogAge = prompt("How old is your dog in human years?");
     var humanAge = dogAge * 7;
     alert("Your dog is " + humanAge + " years old in dog years!");
 }
 calculateDogAge();
+*/
+
+
+//try and use str.include
+// function tacoTuesday(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i].includes("taco")) {
+//             return true;
+//         }
+//     }
+//     return false;
+//}
+
+
+//TODO: Finish coding this program. You need to stop the setInterval() method after the showTime() function has run five times
+
+
+// let count = 0;
+//
+// function showTime() {
+//
+//     // return new date and time
+//     let dateTime = new Date();
+//
+//     // return the time
+//     let time = dateTime.toLocaleTimeString();
+//
+//     count += 1;
+//
+//     //if the count reaches 5, stop the program.
+// if (count > 5) {
+//     clearInterval(display);
+// } else {
+//     console.log(time)
+// }
+// }
+//
+// let display = setInterval(showTime, 5000);
+
+
+function greet(){
+    console.log("Hello World!");
+}
+var delayedGreeting = window.setTimeout(greet, 3000);
+window.clearTimeout(delayedGreeting)
